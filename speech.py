cc1e106093bf310c86c6ae7424253164b676eec7
Sahan Wijayasekara <sahan.nc89@gmail.com>
2020-06-26 10:56:07 +1000

    voice input

--- speech.py
+++ speech.py
@@ -4,3 +4,17 @@ import serial
 
 r = sr.Recognizer()
 r.dynamic_energy_threshold=False
+
+with sr.Microphone() as source:
+    while True:
+        print("Waiting: ")
+        ser=serial.Serial('/dev/ttyACMO', 9600)
+        ser.write("8".encode())
+        audio=r.listen(source)
+        print("done")
+        try:
+            print("You said:"+ r.recognize_google(audio))
+            if r.recognize_google(audio)==("forward"):
+                print("okay")
+                ser=serial.Serial('/dev/ttyACMO',9600)
+                ser.write("7".encode())
